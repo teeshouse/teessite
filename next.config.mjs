d9231dfa12f676822cd49e4a5d944d005ef9f427
Tee's House <info@teeshouse.org>
2026-03-06 19:05:14 -0600

    fix: rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-﻿import type { NextConfig } from "next"
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.sanity.io", pathname: "/images/**" },
